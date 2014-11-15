845acda8e865c0be94ddd4ff70bac8579a6f94ea
Dingyuan Wang <abcdoyle888@gmail.com>
2014-11-15 18:06:07 +0800

    rename folder to jiebazhc

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 setup(name='jiebawenyan',
-      version='0.34',
+      version='0.35.1',
       description='Chinese Words Segementation Utilities',
       author='Sun, Junyi',
       author_email='ccnusjy@gmail.com',
       url='http://github.com/fxsjy',
       packages=['jieba'],
-      package_dir={'jieba':'jieba'},
+      package_dir={'jieba':'jiebazhc'},
       package_data={'jieba':['*.*','finalseg/*','analyse/*','posseg/*']}
 )
